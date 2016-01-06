0d8be2f6af4ce0905813fd6a8d69a75db0f55a74
trosane <trosane@uw.edu>
2016-01-06 10:58:37 -0800

    month array done

--- php/months.php
+++ php/months.php
@@ -1,5 +1,13 @@
 <?php
-    $months = array("J", "F", "M", "A", "May", "J", "July", "Aug", "S", "O", "N", "D");
+    date_default_timezone_set('GMT');
+
+    $months = array();
+
+    for ($i = 1; $i < 13; $i++) {
+        $date = date_create("2015-$i-01");
+        $month = date_format($date, "F");
+        array_push($months, $month);
+    }
     
     sort($months, SORT_STRING);
     foreach ($months as $name) {
