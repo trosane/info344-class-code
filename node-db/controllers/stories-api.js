31f936c7b67e0e4870907fd04ac1cf3d222c0aaa
Trent Rosane <trosane@uw.edu>
2016-01-27 10:21:13 -0800

    hacker news

---
+++ node-db/controllers/stories-api.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+
+module.exports.Router = function(Story) {
+	var router = express.Router();
+	
+	router.get('/stories', function(req, res, next) {
+		//return all stories from the database
+		Story.getAll()
+			.then(function(rows) {
+				res.json(rows);
+			})
+			.catch(next);
+	});
+	
+	router.post('/stories', function(req, res, next) {
+		//insert a new story into the database and 
+		//return the data with default values applied
+		request.get(req.body.url, function(err, response, body) {
+			if (err) {
+				req.body.title = req.body.url;
+			} else {
+				var $ = cheerio.load(body);
+				req.body.title = $('head title').text();
+			}
+			
+			Story.insert(req.body)
+			.then(function(row) {
+				res.json(row);
+			})
+			.catch(next);	
+		});
+	});
+	
+	router.post('/stories/:id/votes', function(req, res, next) {
+		//upvote the story and return the
+		//full stort with current number of votes
+		Story.upVote(req.params.id)
+			.then(function(row) {
+				res.json(row);
+			})
+			.catch(next);
+	});
+	
+	return router;
+};
